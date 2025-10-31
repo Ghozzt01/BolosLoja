8c9f082bcc4f7b731ec8b523682f923eb8122a56
Ghozzt01 <whotaogg55@gmail.com>
2025-10-30 21:26:06 -0300

    comitar ne

--- app/src/main/java/br/com/etecia/myapp/EnviarSenhaActivity.java
+++ app/src/main/java/br/com/etecia/myapp/EnviarSenhaActivity.java
@@ -19,7 +19,7 @@ public class EnviarSenhaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.enviar_senha_layotu);
+        setContentView(R.layout.enviar_senha_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

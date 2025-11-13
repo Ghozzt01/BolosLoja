3cc3716ac775cf19a20b72703c83cbfbf1d67f79
Ghozzt01 <whotaogg55@gmail.com>
2025-11-13 20:21:58 -0300

    mais bolo

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,8 +12,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+
 public class LoginActivity extends AppCompatActivity {
     TextView txtRecuperarSenha;
+    MaterialButton btnCadastrar, btnEntrar;
+
+    TextInputEditText txtEmail, txtSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +33,38 @@ public class LoginActivity extends AppCompatActivity {
         });
 
         txtRecuperarSenha = findViewById(R.id.txtRecuperarSenha);
+        btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnEntrar = findViewById(R.id.btnEntrar);
+
+        txtEmail = findViewById(R.id.txtEmail);
+        txtSenha = findViewById(R.id.txtSenha);
+
+        btnEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = txtEmail.getText().toString();
+                String senha = txtSenha.getText().toString();
+
+                if (email.equals("etecia") && senha.equals("etecia")) {
+                    startActivity(new Intent(getApplicationContext(), MenuPrincipalActivity.class));
+                    finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Usuário ou senha incorretos!!!", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        CadastrarUsuariosActivity.class));
+                finish();
+            }
+        });
+
 
         txtRecuperarSenha.setOnClickListener(new View.OnClickListener() {
             @Override

ac4b2624ff864bb370b3e4e7c62d26566cfbd64c
Ghozzt01 <whotaogg55@gmail.com>
2025-11-27 20:21:28 -0300

    vai tomando

--- app/src/main/java/br/com/etecia/myapp/Pretue.java
+++ app/src/main/java/br/com/etecia/myapp/Pretue.java
@@ -3,6 +3,25 @@ package br.com.etecia.myapp;
 public class Pretue {
     //variaveis globais
     private String titulo;
-
     private int imgTue;
+    //imitando galo
+    public Pretue() {
+    }
+    //criando galo (getter and setter)
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getImgTue() {
+        return imgTue;
+    }
+
+    public void setImgTue(int imgTue) {
+        this.imgTue = imgTue;
+    }
 }
